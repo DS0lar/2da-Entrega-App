ad7efe7d79978323c31cd08b03028c6a528e73b4
Diego <127960201+DS0lar@users.noreply.github.com>
2023-10-19 22:54:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CamaraApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
